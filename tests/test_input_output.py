1ebd6b9da9aa19137ec7c0593664fe08878f2b9f
Sergei Lebedev <superbobry@gmail.com>
2016-07-03 14:37:51 +0300

    Added expect-like tests

---
+++ tests/test_input_output.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import pickle
+import os.path
+
+import pytest
+
+import pyte
+
+
+test_dir = os.path.dirname(__file__)
+
+
+@pytest.mark.parametrize("name", ["vi", "ls", "top", "htop", "mc"])
+def test_input_output(name):
+    with open(os.path.join(test_dir, name + ".input"), "rb") as handle:
+        input = handle.read().decode()
+
+    with open(os.path.join(test_dir, name + ".output"), "rb") as handle:
+        output = pickle.load(handle)
+
+    screen = pyte.Screen(80, 24)
+    stream = pyte.Stream(screen)
+    stream.feed(input)
+    assert screen.display == output
