358dea5b9ea11eeab6c6ed8fb73c220550e17e26
Sergei Lebedev <superbobry@gmail.com>
2017-10-07 23:07:55 +0200

    Meet 0.7.0

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ CLASSIFIERS = [
 
 
 setup(name="pyte",
-      version="0.7.0-dev",
+      version="0.7.0",
       packages=["pyte"],
       install_requires=["wcwidth"],
       setup_requires=["pytest-runner"],
